6c522ad4c55878f9dc7ac38c4d7e380b682a0385
jaeyoung <lyoung09>
2021-11-25 00:41:08 +0900

    1125ios

--- forfor/lib/utils/utils.dart
+++ forfor/lib/utils/utils.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class Utils {
-  static void showSnackbar(BuildContext context, {required String message}) {
-    final snackbar = SnackBar(
-      content: Text(message, style: TextStyle(fontSize: 20)),
-      backgroundColor: Colors.red,
-    );
+// class Utils {
+//   static void showSnackbar(BuildContext context, {required String message}) {
+//     final snackbar = SnackBar(
+//       content: Text(message, style: TextStyle(fontSize: 20)),
+//       backgroundColor: Colors.red,
+//     );
 
-    Scaffold.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(snackbar);
-  }
-}
+//     Scaffold.of(context)
+//       ..removeCurrentSnackBar()
+//       ..showSnackBar(snackbar);
+//   }
+// }
